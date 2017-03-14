3183b41e1165bde48e55ecf9b88582395e951323
Chad Peterson <chapeter@cisco.com>
2017-03-14 14:10:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # from email.mime.text import MIMEText
 # from email.mime.multipart import MIMEMultipart
 
-version = '0.4.1'
+version = '0.4.2'
 
 token = os.environ['SPARK_BOT_TOKEN']
 url = 'https://api.ciscospark.com'
@@ -232,29 +232,26 @@ def injest():
             message_text = message.attributes['text']
 
             msg = message_text.split(name)
-            sys.stderr.write("removing {} from message".format(name))
-            x=0
-            for m in msg:
-                sys.stderr.write("{0} - {1}".format(x,m))
-                x=x+1
+            sys.stderr.write("\nremoving {} from message\n".format(name))
             msg = msg[1].strip()
             sys.stderr.write("\nMessage is - {}\n".format(msg))
-            
-            
 
-
-            if msg.split()[0] == '-version':
-                response = version
-                spark_msg = version
-            elif msg.split()[0] == '-email':
-                response = buildEmail(message, msg, sender)
-                spark_msg = response + "\nYou no longer need to tag messages with -email, just speak to me"
-            elif msg.split()[0] == 'help':
-                response = help()
-                spark_msg = response
+            if len(msg) < 1:
+                sys.stderr.write("\nMessage is empty\n")
+                spark_msg = "Please type a message to be sent"
             else:
-                response = buildEmail(message, msg, sender)
-                spark_msg = "Email Sent"
+                if msg.split()[0] == '-version':
+                    response = version
+                    spark_msg = version
+                elif msg.split()[0] == '-email':
+                    response = buildEmail(message, msg, sender)
+                    spark_msg = response + "\nYou no longer need to tag messages with -email, just speak to me"
+                elif msg.split()[0] == 'help':
+                    response = help()
+                    spark_msg = response
+                else:
+                    response = buildEmail(message, msg, sender)
+                    spark_msg = "Email Sent"
 
             room.send_message(session, spark_msg)
 
