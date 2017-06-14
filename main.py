15b86c58802aaf537dc21c5b68dcf7857822393f
Chad Peterson <chapeter@cisco.com>
2017-06-14 15:45:18 -0500

    Create main.py

--- main.py
+++ main.py
@@ -159,19 +159,23 @@ def getRecipients(message, excludelist):
     users = getUsers(roomid)
     sys.stderr.write("Inside getRecipients: User List\n----\n")
     valid_users = []
-    for user in users:
-        sys.stderr.write(user + "\n")
-    for useraddress in users:
-        sys.stderr.write("\nLooking at {}'s address\n".format(useraddress))
-        for domain in excludelist:
-            sys.stderr.write("Looking for {0} in {1}'s address\n".format(domain, useraddress))
-            if domain in useraddress:
-                sys.stderr.write("Found {0} in {1}.  Removing it from list.\n".format(domain, useraddress))
-                #users.remove(useraddress)
-                #sys.stderr.write("Removed")
-            else:
-                sys.stderr.write("Did not find {0} in {1}. Adding it to valid list\n".format(domain, useraddress))
-                valid_users.append(useraddress)
+    if not excludelist:
+        sys.stderr.write("Empty exclude list, all users valid")
+        valid_users = users
+    else:
+        for user in users:
+            sys.stderr.write(user + "\n")
+        for useraddress in users:
+            sys.stderr.write("\nLooking at {}'s address\n".format(useraddress))
+            for domain in excludelist:
+                sys.stderr.write("Looking for {0} in {1}'s address\n".format(domain, useraddress))
+                if domain in useraddress:
+                    sys.stderr.write("Found {0} in {1}.  Removing it from list.\n".format(domain, useraddress))
+                    #users.remove(useraddress)
+                    #sys.stderr.write("Removed")
+                else:
+                    sys.stderr.write("Did not find {0} in {1}. Adding it to valid list\n".format(domain, useraddress))
+                    valid_users.append(useraddress)
     return valid_users
 
 def getExcludelist(message_text):
